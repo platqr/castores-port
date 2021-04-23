ad17308aa1ffa804c3159a5ce9daedeaab4567d4
Miguel Bui <miguelbuir@gmail.com>
2021-04-22 18:11:35 -0600

    implemented touch controls

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class Player : MonoBehaviour
     private Main_Scripts _mainScript;
     
     [SerializeField] private Animator _animator;
+
+    [SerializeField] private bool _uiJump;
+    private bool _usingUiButtons;
 
     [SerializeField] private LayerMask platformLayerMask;
     [SerializeField] private Rigidbody2D rb;
@@ -23,7 +27,9 @@ public class Player : MonoBehaviour
     }
 
     private void Update(){
-        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        if(!_usingUiButtons){
+            horizontalMovement = Input.GetAxisRaw("Horizontal");
+        }
         Jump();
         Fall();
         Animations();
@@ -34,7 +40,7 @@ public class Player : MonoBehaviour
     }
 
     private void Jump(){
-        if (IsGrounded() && Input.GetButtonDown("Jump"))
+        if (IsGrounded() && Input.GetButtonDown("Jump") || IsGrounded() &&  _uiJump)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
@@ -84,6 +90,29 @@ public class Player : MonoBehaviour
         if (transform.position.y <= -5.5f){
             _mainScript.GameOver();
         }
+    }
+
+
+    public void UiLeft(){
+        if (horizontalMovement != 0){
+            horizontalMovement = 0;
+        } else if(horizontalMovement !=-1){
+            horizontalMovement = -1;
+        }
+        _usingUiButtons = !_usingUiButtons;
+    }
+
+    public void UiRight(){
+        if (horizontalMovement != 0){
+            horizontalMovement = 0;
+        } else if(horizontalMovement !=1){
+            horizontalMovement = 1;
+        }
+        _usingUiButtons = !_usingUiButtons;
+    }
+
+    public void UiJump(){
+        _uiJump = !_uiJump;
     }
 
 }
